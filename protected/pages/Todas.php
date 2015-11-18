a60418de565c723dff8dcd5ed0fc24883b8c123f
algerion <orctek-varios@yahoo.com.mx>
2015-11-18 17:31:02 -0600

    Agregado de foto de regalo
    Código para despliegue de la imagen del regalo

--- protected/pages/Todas.php
+++ protected/pages/Todas.php
@@ -11,7 +11,12 @@ class Todas extends TPage
 
 		$this->cn = new DbCon($this, "db");
 
-		$consulta = "SELECT *, CONCAT('<a target=\"inter\" href=\"index.php?page=Inter&codigo=', codigo, '\">' , codigo, \"</a>\") AS codigo, CONCAT('<img src=\"fotos/', codigo, foto, '\" style=\"max-width: 100px;\"/>') AS foto FROM datos ORDER BY codigo";
+		$consulta = "SELECT *, CONCAT('<a target=\"inter\" href=\"index.php?page=Inter&codigo=', codigo," .
+				"'\">' , codigo, \"</a>\") AS codigo, CONCAT('<img src=\"fotos/', codigo, foto, '\" " .
+				"style=\"max-width: 100px;\"/>') AS foto, CASE WHEN regalo IS NOT NULL AND regalo <> '' " .
+				"THEN CONCAT('<img src=\"regalos/', " .
+				"codigo, regalo, '\" style=\"max-width: 100px;\"/>') ELSE '' END " .
+				"AS regalo FROM datos ORDER BY codigo";
 		$result = $this->cn->consulta($consulta);
 		$this->dgTodas->DataSource = $result;
 		$this->dgTodas->dataBind();
