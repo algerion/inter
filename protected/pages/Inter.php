b85bc4771e14bbb56d0e011760eb43847157de2d
algerion <orctek-varios@yahoo.com.mx>
2015-11-18 14:48:23 -0600

    Agregado código de rastreo y foto
    Agregado guardado del código de rastreo y de la foto

--- protected/pages/Inter.php
+++ protected/pages/Inter.php
@@ -1,9 +1,11 @@
 <?php
+Prado::using('System.Web.UI.ActiveControls.*');
 include_once('../compartidos/clases/DbCon.php');
 
 class Inter extends TPage
 {
 	private $cn;
+	private $codigo;
 
 	public function onLoad($param)
 	{
@@ -11,8 +13,10 @@ class Inter extends TPage
 
 		$this->cn = new DbCon($this, "db");
 
+		$this->codigo = $this->Request["codigo"];
+		
 		$consulta = "SELECT id_persona FROM datos WHERE codigo = :codigo";
-		$parametros = array("codigo"=>$this->Request["codigo"]);
+		$parametros = array("codigo"=>$this->codigo);
 		$result = $this->cn->consulta($consulta, $parametros);
 		if(count($result) > 0)
 		{
@@ -32,11 +36,15 @@ class Inter extends TPage
 		$dom = ($signo == "<") ? "domicilio, sugerencias, " : "";
 
 		$foto = " CONCAT('<img src=\"fotos/', codigo, foto, '\" style=\"max-width: 100px;\"/>') AS foto ";
-		$con = "SELECT nombre, movil, nacimiento, " . $dom . $foto . " FROM datos WHERE codigo ";
+		$regalo = ", CASE WHEN regalo IS NOT NULL AND regalo <> '' THEN CONCAT('<img src=\"regalos/', " .
+				"codigo, regalo, '\" style=\"max-width: 100px;\"/>') ELSE '' END " .
+				"AS regalo, cod_rastreo ";
+		$reg = ($signo == "<") ? "" : $regalo;
+		$con = "SELECT nombre, movil, nacimiento, " . $dom . $foto . $reg . " FROM datos WHERE codigo ";
 		$order = " ORDER BY codigo " . $desc . " LIMIT 1";
 	
 		$consulta = $con . $signo . " :codigo" . $order;
-		$parametros = array("codigo"=>$this->Request["codigo"]);
+		$parametros = array("codigo"=>$this->codigo);
 		$result = $this->cn->consulta($consulta, $parametros);
 		if(count($result) < 1)
 		{
@@ -47,5 +55,30 @@ class Inter extends TPage
 		$grid->dataBind();
 	}
 	
+	public function fileUploaded($sender, $param)
+    {
+        if($sender->HasFile)
+			$sender->saveAs("regalos/" . $this->codigo . $sender->FileName);
+    }
+	
+	public function btnGuardar_Click($sender, $param)
+	{
+		mt_srand(time());
+		$parametros = array(
+			"cod_rastreo"=>$this->txtRastreo->Text,
+			"mail"=>$this->hidMail->Value
+		);
+        if($this->uplFoto->HasFile)
+			$parametros = array_merge(
+				$parametros,
+				array("regalo"=>$this->uplFoto->FileName)
+			);
+		$busqueda = array("codigo"=>$this->codigo);
+		$this->cn->actualiza("datos", $parametros, $busqueda);
+		$this->ClientScript->registerBeginScript("guardado",
+				"alert('Sus datos han sido almacenados correctamente');\n"
+				. "document.location.href = 'index.php?page=Inter&codigo=" . $this->codigo . "';\n"
+		);
+	}
 }
 ?>
